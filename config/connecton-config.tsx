52663f775a79ac8e650571a1b794c5b23995f72a
Khabubu Ndivhuwo <khabubundivhuwo@gmail.com>
2025-03-28 19:00:52 +0200

    feat: add connection configuration for Google Sheets, Gmail, and OpenAI

---
+++ config/connecton-config.tsx
@@ -0,0 +1,39 @@
+import { Icons } from '@/components/ui/icons';
+import { Connection } from '@/types/connection';
+import { LucideProps } from 'lucide-react';
+
+export const connectionConfig: Connection[] = [
+  {
+    icon: (props: LucideProps) => <Icons.googleSheets {...props} />,
+    displayName: 'Google Sheets',
+    name: 'google-sheets',
+    description: 'Configure your Google Sheets to sync or create data.',
+    link: '/api/server/connections/connect/google-sheet',
+    categories: ['Google', 'Data Storage'],
+    type: 'oauth2',
+  },
+  {
+    icon: (props: LucideProps) => <Icons.gmail {...props} />,
+    displayName: 'Gmail',
+    name: 'gmail',
+    description: 'Configure your Gmail to sync or create emails.',
+    link: '/api/server/connections/connect/gmail',
+    categories: ['Google', 'Email'],
+    type: 'oauth2',
+  },
+  {
+    icon: (props: LucideProps) => <Icons.openai {...props} />,
+    displayName: 'OpenAI',
+    name: 'openai',
+    description: 'Configure your OpenAI to generate text.',
+    categories: ['AI', 'Text Generation'],
+    type: 'credentials',
+    inputs: [
+      {
+        name: 'apiKey',
+        label: 'Open API Key',
+        type: 'text',
+      },
+    ],
+  },
+];
