b00b08383bf03215f464ec2a19bbb1c1327c9f8f
Khabubu Ndivhuwo <khabubundivhuwo@gmail.com>
2025-03-31 22:09:18 +0200

    feat: add GridPattern component and integrate it into Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { GridPattern } from '@/components/shared/grid-pattern';
 import { MarketingPageFooter } from '@/components/shared/marketing-page-footer';
 import { MarketingPageHeader } from '@/components/shared/marketing-page-header';
+import { cn } from '@/lib/utils';
 
 export default function Home() {
   return (
@@ -11,6 +13,25 @@ export default function Home() {
             <div className="space-y-1 text-center"></div>
           </div>
         </div>
+        <GridPattern
+          squares={[
+            [5, 12],
+            [6, 16],
+            [3, 20],
+            [8, 23],
+            [2, 25],
+            [15, 15],
+            [17, 16],
+            [20, 20],
+            [13, 20],
+            [25, 25],
+            [16, 27],
+          ]}
+          className={cn(
+            '[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]',
+            '-inset-y-1/2 inset-x-0 h-[200%] skew-y-12',
+          )}
+        />
         <MarketingPageFooter />
       </div>
     </div>
